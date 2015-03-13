40bed39e07d1debc2eaa87a4edb34362030882a2
goldsun27 <todayhonest27@gmail.com>
2015-03-13 12:26:55 -0700

    packages installed, gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = function(grunt) {
+  
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.laodNpmTasks('grunt-karma');
+
+  grunt.initConfig({
+    
+  });
+};
